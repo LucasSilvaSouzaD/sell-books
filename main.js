c6fefb2fcaea21b89db1f78c10f618723c256b4c
Lucas Silva Souza <lucassilvadonato23@gmail.com>
2021-11-30 08:00:33 -0300

    atualizando

--- main.js
+++ main.js
@@ -12,33 +12,37 @@ const questionFunc = terminal.question.bind(terminal);
 const questionAsync = msg => functionToPromise(questionFunc, `${msg}\n`)
 // critérios de venda
 const critery = [
-    {'qtdMinimum': 2, 'qtdMaximum': 4, 'prcBook': 0.25, 'prcFixed': 7.50},
-    {'qtdMinimum': 5, 'qtdMaximum': 7, 'prcBook': 0.50, 'prcFixed': 2.50},
-    {'qtdMinimum': 8, 'qtdMaximum': 10, 'prcBook': 0.65, 'prcFixed': 1.50},
+    { 'prcBook': 0.25, 'prcFixed': 7.50 },
+    { 'prcBook': 0.50, 'prcFixed': 2.50 },
+    { 'prcBook': 0.65, 'prcFixed': 1.50 },
 ]
-// preço acima de 10 livros
-const offerAboveTen = 0.50;
-// preço para um livro
-const oneBook = 10.00;
 
 // filtrar e calcular critério de venda
 const calcCritery = async (qtdBooks, arrayCritery) => {
-    const criteryFilter = await arrayCritery.filter(item => qtdBooks >= item.qtdMinimum && qtdBooks <= item.qtdMaximum);
-    return criteryFilter ? criteryFilter[0].prcBook * qtdBooks + criteryFilter[0].prcFixed 
-    : parseInt(qtdBooks) > 10 ? qtdBooks * offerAboveTen 
-    : oneBook;  
+    const totalCriterys = arrayCritery.map(critery => Object.assign(critery, { total: (critery.prcBook * qtdBooks) + critery.prcFixed }))
+    console.log(totalCriterys)
+
+    const totals = totalCriterys.map((critery) => critery.total)
+    const removeNumberRep = [... new Set(totals)]
+
+    let difference = removeNumberRep.filter(x => !totals.includes(x));
+    console.log(difference)
+        // const criteryFilter = await arrayCritery.filter(item => qtdBooks >= item.qtdMinimum && qtdBooks <= item.qtdMaximum);
+    // return criteryFilter ? criteryFilter[0].prcBook * qtdBooks + criteryFilter[0].prcFixed
+    //     : parseInt(qtdBooks) > 10 ? qtdBooks * offerAboveTen
+    //         : oneBook;
 }
 
-;
-(async function main(){
+    ;
+(async function main() {
     try {
         const qtdBooks = await questionAsync('Digite a quantidade de livros que deseja comprar:');
         const result = await calcCritery(qtdBooks, critery);
-        console.log(`R$ ${result}`)
+        console.log(result)
     } catch (error) {
         console.log(error);
     } finally {
         terminal.close()
     }
-    
+
 })()
