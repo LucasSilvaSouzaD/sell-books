060578ac6f512fb5205fd661fa213b4ff86c6692
Lucas Silva Souza <lucassilvadonato23@gmail.com>
2022-07-27 15:04:04 -0300

    att

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const functionToPromise = (func, ...args) => {
 
 const questionFunc = terminal.question.bind(terminal);
 const questionAsync = msg => functionToPromise(questionFunc, `${msg}\n`)
-// critérios de venda
+
 const critery = [
     { 'prcBook': 0.25, 'prcFixed': 7.50 },
     { 'prcBook': 0.50, 'prcFixed': 2.50 },
@@ -25,20 +25,12 @@ function hasDuplicate(array) {
     return array.filter((e, i, a) => a.indexOf(e) !== i)
 }
 
-
 const criteryCalculator = async (qtdBooks, arrayCritery) => {
+
     const totalCriterys = arrayCritery.map(critery => Object.assign(critery, { total: (critery.prcBook * qtdBooks) + critery.prcFixed }))
-    
-    // separar o retorno dos calculos em matriz
     const criteryTotals = await totalArray(totalCriterys);
-
-    // retornar o duplicado caso tenha
     const hasRepeatCritery = await hasDuplicate(criteryTotals);
-
-    // máximo do retorno
     const maxMatriz = await Math.max.apply(null, criteryTotals );
-
-    // caso tenha o valor repetido retorna ele, se não retorna o máximo dos critério
     const criteryFix = hasRepeatCritery.length > 0 ? hasRepeatCritery[0] : maxMatriz
 
     return console.log(`final: ${criteryFix}`)
@@ -48,8 +40,8 @@ const criteryCalculator = async (qtdBooks, arrayCritery) => {
 (async function main() {
     try {
         const qtdBooks = await questionAsync('Digite a quantidade de livros que deseja comprar:');
+
         return await criteryCalculator(qtdBooks, critery);
-        // console.log(result)
     } catch (error) {
         console.log(error);
     } finally {
